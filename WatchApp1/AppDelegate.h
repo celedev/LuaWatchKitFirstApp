17095a88ad4aa26f541aec2a1aed9ac019a752dc
Jean-Luc Jumpertz <jean-luc@celedev.eu>
2014-11-20 11:08:41 +0100

    Initial Commit

---
+++ WatchApp1/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  WatchApp1
+//
+//  Created by Jean-Luc on 20/11/2014.
+//  Copyright (c) 2014 Celedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ WatchApp1/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  WatchApp1
+//
+//  Created by Jean-Luc on 20/11/2014.
+//  Copyright (c) 2014 Celedev. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
